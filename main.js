c8e17a5355c2b1eab5bebd9e433c936460ae7fa9
MaoYingrong <65118291+MaoYingrong@users.noreply.github.com>
2020-12-03 22:48:36 +0800

    main.js里面你改过的我之前没有run出来，然后我就先把它comment了用原来的，你可以去掉comment直接改就行。以及我调了一些参数.....

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const attack = d3.csv("attack.csv");
 console.log(attack);
 console.log(typeof attack);
 
-d3.csv("globalterrorismdb_0718dist.csv").then(function (data) {
+d3.csv("attack.csv").then(function (data) {
   data.forEach((d) => {
     d.iyear = +d.iyear;
     d.attacktype1 = +d.attacktype1;
@@ -16,20 +16,23 @@ d3.csv("globalterrorismdb_0718dist.csv").then(function (data) {
   });
 });
 
-d3.json("world_map.json").then((data) => {
+
+
+
+ d3.json('world_map.json').then((data)=>{
   //console.log(data.objects);
   const countries = topojson.feature(data, data.objects.countries);
   //console.log(countries);
-
-  const projection = d3
-    .geoMercator()
-    .scale(110)
-    .center([10, 0])
-    .rotate([0, 0])
-    .translate([width / 2, height / 2 + 20]);
+  
+  const projection = d3.geoMercator()
+  .scale(135)
+  .center([-11, 0])
+  .rotate([0, 0])
+  .translate([width/2, height/2]);
 
   const pathGenerator = d3.geoPath().projection(projection);
-
+  
+  /*
   svg
     .append("path")
     .attr("class", "sphere")
@@ -44,4 +47,96 @@ d3.json("world_map.json").then((data) => {
     .append("path")
     .attr("class", "countries")
     .attr("d", (d) => pathGenerator(d));
-});
+
+  let width = 380;
+  let height = 350;
+  */
+
+  let worldmap = d3.select("svg.mappp")
+
+  worldmap.append('path')
+    .attr("class", "sphere")
+    .attr('d', pathGenerator({type:'Sphere'}));
+ 
+  
+    //console.log(pathGenerator.bounds(countries));
+ 
+
+  console.log(pathGenerator({type:'Sphere'}));
+  const paths = worldmap.selectAll('path')
+    .data(countries.features);
+  paths.enter().append('path')
+    .attr("class", "countries")
+    .attr('d', d => pathGenerator(d));
+ });
+
+ 
+//The linechart start from here
+d3.csv("data2.csv").then(function(data) {
+  
+    data.forEach(d => {
+        d.year = d.year;
+        d.country = d.country;
+        d.number = +d.data;
+    });
+ 
+  let g = d3.select("svg.linechart").append("g")
+          .attr("transform", "translate(" + 100 + "," + 200 + ")")
+          .attr("class","linechart");
+
+  var x = d3.scaleLinear()
+    .domain([1970,2020])
+    .range([0,430]);
+  g.append("g")
+    .attr("transform", "translate(0," + 300 + ")")
+    .call(d3.axisBottom(x));
+
+  var y = d3.scaleLinear()
+    .domain([0, 400])
+    .range([300, 0])
+  g.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add the valueline path.
+  //console.log(data.)
+  g.append("path")
+      .datum(data.filter(d => d.country == "Philippines"))
+      .attr("class", "line")
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.year) })
+        .y(function(d) { return y(d.number) }))
+
+  
+  
+  });
+/*
+  g.append('g')
+      .attr("transform", "translate(0," + 400 + ")")
+      .attr('class', 'x-axis')
+      .call(xAxis);
+  g.append('g')
+      .attr("transform", "translate(0," + 100 + ")")
+      .attr('class', 'y-axis')
+      .call(yAxis);
+
+  g.append("path")
+      .datum(d)
+      .data(data.filter(d => d.month == m))
+  
+  svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
+
+*/
+
+
+
