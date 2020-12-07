8b45ca8ddb410b626a4258862997f9c98e0c6dc0
MaoYingrong <65118291+MaoYingrong@users.noreply.github.com>
2020-12-07 10:35:57 +0800

    the right version

--- main.js
+++ main.js
@@ -87,7 +87,6 @@ async function draw_map1(year = 2000) {
     .on("mouseout", function (d) {
       div.transition().duration(500).style("opacity", 0);
     })
-<<<<<<< HEAD
     .on("click", function(path,d){
       var name = d.properties.name;
       one_country = d3.select(this);
@@ -100,8 +99,7 @@ async function draw_map1(year = 2000) {
     
   
     };
-=======
-    //.on("mousedown", function(d){paths.attr("fill","blue")});
+    
 
     // slider
     var slider = d3.select('#slider');
@@ -112,8 +110,6 @@ async function draw_map1(year = 2000) {
         draw_map1(year);
         d3.select("output#slidertext").text(year);
     });
-}
->>>>>>> 0eab0a613b49109b0a1c9054939544cd19a25c16
 
 
 //draw the map with points
@@ -150,10 +146,7 @@ worldmap
       .attr("fill","white")
       .attr("stroke","black")
       .attr('d', d => pathGenerator(d))
-<<<<<<< HEAD
       
-      .on("mousedown", function(event,d){
-      d3.select(this).attr('fill', 'blue')})
 
       
       .on("mouseover", function (path, d) {
@@ -164,21 +157,13 @@ worldmap
             .style("left", event.pageX + "px")
             .style("top", event.pageY - 28 + "px");
         })
-      .on("mouseup", function(event,d){
-          d3.select(this).attr('fill', 'red')})
-
-       doucument.onmousemove = function(event){
-=======
-      /*
-      doucument.onmousemove = function(event){
->>>>>>> 0eab0a613b49109b0a1c9054939544cd19a25c16
-          event = event || window.event;
-          var left = event.clientX;
-          var top = event.clientY;
-        
-          paths.style.left = left+"px";
-          paths.style.top = top+"px";
-        };
+        .on("click", function(path,d){
+          var name = d.properties.name;
+          one_country = d3.select(this);
+          one_country.style("stroke","blue");
+          one_country.style("stroke-width",5);
+          one_country.attr("class","markedone")
+          draw_linechart(name);
       });
     
 
@@ -219,10 +204,9 @@ worldmap
         div.transition().duration(500).style("opacity", 0);
       });
 
-<<<<<<< HEAD
   });
-}
-=======
+});}
+
     // slider
     var slider = d3.select('#slider');
     slider.on('change', function() {
@@ -234,11 +218,20 @@ worldmap
         d3.select("output#slidertext").text(year);
     });
 
-  })
-  };
->>>>>>> 0eab0a613b49109b0a1c9054939544cd19a25c16
-
 // The linechart starts from here
+let g = d3
+    .select("svg.linechart")
+    .append("g")
+    .attr("transform", "translate(" + 100 + "," + 290 + ")")
+    .attr("class", "linechart");
+
+  var x = d3.scaleLinear().domain([1970, 2020]).range([0, 430]);
+  g.append("g")
+    .attr("transform", "translate(0," + 300 + ")")
+    .call(d3.axisBottom(x));
+
+  var y = d3.scaleLinear().domain([0, 400]).range([300, 0]);
+  g.append("g").call(d3.axisLeft(y));
 
 function draw_linechart(country_name){
 d3.csv("data2.csv").then(function (data) {
@@ -290,7 +283,8 @@ d3.csv("data2.csv").then(function (data) {
 function clear_data(){
   d3.selectAll(".line").remove();
   marked = d3.selectAll(".markedone")
-  .style("stroke-width",0)
+  .style("stroke-width",1)
+  .style("stroke",black)
   .attr("class","countries");
 }
 
